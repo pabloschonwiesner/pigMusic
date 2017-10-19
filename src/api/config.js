994e5270fff808212a0580d0f613a32d1ad5122b
Pablo <pabloschonwiesner@hotmail.com>
2017-10-19 14:38:54 -0300

    Inicial commit

---
+++ src/api/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+	appname: 'platzimusic',
+	apiKey: '66b51719c274b2fffa05b5d6d7d152dc',
+	secret: '02dd070d5e2d20c44f3374a59e3d083e',
+	registredTo: 'sachitaplatzi'
+}
+
+/*http://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=argentina&api_key=66b51719c274b2fffa05b5d6d7d152dc&format=json`*/
